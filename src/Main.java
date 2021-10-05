b0bc121ab823c140da60747f49d70a15ce3321e5
Azize Zinadinova <azizeshka87@gmail.ru>
2021-10-05 23:07:02 +0300

    jhg

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 10_000;
+        int divider = 20;
+        int miles = TicketPrice / divider;
+
+        System.out.println("Ваш бонус составит "  +  miles  +  " миль" );
+    }
+}
